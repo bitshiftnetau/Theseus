091247988071c2951cbf261a45ca08964c078d18
bitshiftnetau <admin@bitshift.net.au>
2019-06-08 01:11:11 +1000

    efm32zg_app.c/h: added timer_Delay to application layer

---
+++ src/application/app_fns/efm32zg_app.h
@@ -0,0 +1,14 @@
+#ifndef EFM32ZG_APP
+#define EFM32ZG_APP
+
+#include <stdint.h>
+
+
+/*******************************
+ *          TIMER
+ ******************************/
+
+int timer_Delay(void* host_tr, uint32_t dlyTicks);
+
+
+#endif
