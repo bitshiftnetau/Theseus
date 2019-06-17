1d7d454fdff7e0bb34687a78c2ec530b4ad4d12e
bitshiftnetau <admin@bitshift.net.au>
2019-06-17 15:57:35 +1000

    _app_config.h: add includes for x_adaptor and x_types files

--- src/application/configs/_app_config.h
+++ src/application/configs/_app_config.h
@@ -10,14 +10,19 @@
 
 #ifdef EFM32ZG222F32
   #include "config_efm32zg222f32.h"
+  #include "efm32zg222f32_adaptor.h"
+  #include "efm32zg_types_HAL.h"
 #endif
 
 #ifdef DW1000
   #include "config_dw1000.h"
+  #include "dw1000_adaptor.h"
+  #include "dw1000_types.h"
 #endif
 
 #ifdef VENUS638
   #include "config_venus638.h"
+  #include "venus638_adaptor.h"
 #endif
 
 
