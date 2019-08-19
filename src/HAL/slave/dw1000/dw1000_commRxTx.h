400ef68f64052ac87a62b804cae97ea0d7f96b11
bitshiftnetau <admin@bitshift.net.au>
2019-08-19 19:50:51 +1000

    dw1000_commRxTx.h: redefined parameter DW_nodelist* dw_nodelist to void* ext_dev_object in dw_Rx() and dw_Tx()

--- src/HAL/slave/dw1000/dw1000_commRxTx.h
+++ src/HAL/slave/dw1000/dw1000_commRxTx.h
@@ -9,7 +9,7 @@ extern uint32_t(*dw_comm_table[])();
 extern void(* dw_PolAlphaDeAssert[DW_SPI_POLALPA_VARIANTS])();
 extern void(* dw_PolAlphaAssert[DW_SPI_POLALPA_VARIANTS])();
 
-uint32_t dw_Rx(void* host_object, int(*host_usart)(), DW_nodelist* dw_nodelist, uint8_t* buffer_in, uint32_t buffer_len);
-uint32_t dw_Tx(void* host_object, int(*host_usart)(), DW_nodelist* dw_nodelist, uint8_t* buffer_in, uint32_t buffer_len);
+uint32_t dw_Rx(void* host_object, int(*host_usart)(), void* ext_dev_object, uint8_t* buffer_in, uint32_t buffer_len);
+uint32_t dw_Tx(void* host_object, int(*host_usart)(), void* ext_dev_object, uint8_t* buffer_in, uint32_t buffer_len);
 
 #endif
