87149a5e48dfae8aad1c45f3110f7122314eded6
bitshiftnetau <admin@bitshift.net.au>
2019-08-19 19:36:14 +1000

    _app_fns.c/h: timer_Delay() defined

---
+++ src/application/app_fns/_app_fns.h
@@ -0,0 +1,14 @@
+#ifndef _APP_FNS
+#define _APP_FNS
+
+#include <stdint.h>
+
+
+/*******************************
+ *          TIMER
+ ******************************/
+
+int timer_Delay(uint32_t dlyTicks);
+
+
+#endif
