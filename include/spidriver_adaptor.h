ef78f6c534cddc8884df9f98fd8178a78e66e346
bitshiftnetau <admin@bitshift.net.au>
2020-01-29 17:05:49 +1100

    add lib and include folders with spidriver files

---
+++ include/spidriver_adaptor.h
@@ -0,0 +1,11 @@
+#ifndef SPIDRIVER_ADAPTOR_H
+#define SPIDRIVER_ADAPTOR_H
+
+#include <stdint.h>
+
+#define SD_CONFIG_INDEX 0
+
+int sd_Init(void* host_object, int(*host_usart)(), void* ext_dev_object);
+int sd_Data(void* host_object, int(*host_usart)(), void* ext_dev_object, uint32_t read_write);
+
+#endif
